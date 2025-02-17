0cc15375014d39bfb58fafdfba34b276ed239367
hjlee1107 <pigmun2@naver.com>
2025-02-17 10:29:17 +0900

    change

--- themes/krds-theme/assets/js/egovframe.js
+++
@@ -1,6 +0,0 @@
-// 페이지 상단 이동 버튼
-const container = document.getElementById("container");
-const topBtn = container.querySelector(".page-top-button");
-topBtn.addEventListener("click",function(){
-    location.href="#";
-});
